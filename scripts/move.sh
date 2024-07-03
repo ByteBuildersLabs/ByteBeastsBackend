048a0c8313d19783ffac607479f4c8d01c6135ec
Marco Araya Jiménez <75222804+coxmars@users.noreply.github.com>
2024-07-03 13:47:32 +0200

    Local is working

--- scripts/move.sh
+++ scripts/move.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 pushd $(dirname "$0")/..
 
-export RPC_URL="https://api.cartridge.gg/x/work/katana";
+export _URL="http://0.0.0.0:5050";
 
 export WORLD_ADDRESS=$(cat ./manifests/dev/manifest.json | jq -r '.world.address')
 
